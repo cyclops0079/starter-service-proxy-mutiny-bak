2218f865f1b9c3422490bd3e28c9ba1f3da87044
cyclops0079 <123039341+cyclops0079@users.noreply.github.com>
2023-02-12 12:17:10 +0800

    Update BarmanService.java

--- src/main/java/com/example/beers/BarmanService.java
+++ src/main/java/com/example/beers/BarmanService.java
@@ -1,20 +1,4 @@
+@ModuleGen(groupPackage = "com.example", name = "beers")
 package com.example.beers;
 
-import io.vertx.codegen.annotations.ProxyGen;
-import io.vertx.codegen.annotations.VertxGen;
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-
-@VertxGen
-@ProxyGen
-public interface BarmanService {
-
-    Future<Beer> giveMeAStaticBeer(String customerName);
-    Future<Integer> getMyBill(String customerName);
-    Future<Void> payMyBill(String customerName);
-
-    static BarmanService createProxy(Vertx vertx, String address) {
-        return new BarmanServiceVertxEBProxy(vertx, address);
-    }
-
-}
+import io.vertx.codegen.annotations.ModuleGen;
