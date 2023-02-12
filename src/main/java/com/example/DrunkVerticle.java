0234fd8f1cb3ef3bb5a5531d08209bd3a947e667
cyclops0079 <123039341+cyclops0079@users.noreply.github.com>
2023-02-12 12:08:35 +0800

    feat: Use Future on service interface/implimentation

--- src/main/java/com/example/DrunkVerticle.java
+++ src/main/java/com/example/DrunkVerticle.java
@@ -11,21 +11,22 @@ public class DrunkVerticle extends AbstractVerticle {
         BarmanService barmanService = BarmanService.createProxy(vertx, "beers.services.myapplication");
 
         System.out.println("The barman is ready to serve you");
-        barmanService.giveMeAStaticBeer("homer", b1 -> {
-            if (b1.failed()) {
-                System.err.println("Cannot get my first beer!");
-                startPromise.fail(b1.cause());
-                return;
-            }
-            System.out.println("My beer is a " + b1.result() + " and it costs " + b1.result().getPrice() + "€");
-            vertx.setTimer(10, l -> {
-                barmanService.getMyBill("homer", billAr -> {
-                    System.out.println("My bill with the bar is " + billAr.result());
-                    barmanService.payMyBill("homer");
-                    startPromise.complete();
+        barmanService.giveMeAStaticBeer("homer")
+            .onComplete(b1 -> {
+                if (b1.failed()) {
+                    System.err.println("Cannot get my first beer!");
+                    startPromise.fail(b1.cause());
+                    return;
+                }
+                System.out.println("My beer is a " + b1.result() + " and it costs " + b1.result().getPrice() + "€");
+                vertx.setTimer(10, l -> {
+                    barmanService.getMyBill("homer").onComplete(billAr -> {
+                        System.out.println("My bill with the bar is " + billAr.result());
+                        barmanService.payMyBill("homer");
+                        startPromise.complete();
+                    });
                 });
             });
-        });
 
     }
 
