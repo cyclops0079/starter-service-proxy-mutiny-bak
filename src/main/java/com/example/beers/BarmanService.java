8cd454cf243120379a68667de9725c61aacc07eb
cyclops0079 <123039341+cyclops0079@users.noreply.github.com>
2023-02-12 12:00:33 +0800

    Update BarmanService.java

--- src/main/java/com/example/beers/BarmanService.java
+++ src/main/java/com/example/beers/BarmanService.java
@@ -2,17 +2,16 @@ package com.example.beers;
 
 import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.codegen.annotations.VertxGen;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 
 @VertxGen
 @ProxyGen
 public interface BarmanService {
 
-    void giveMeAStaticBeer(String customerName, Handler<AsyncResult<Beer>> handler);
-    void getMyBill(String customerName, Handler<AsyncResult<Integer>> handler);
-    void payMyBill(String customerName);
+    Future<Beer> giveMeAStaticBeer(String customerName);
+    Future<Integer> getMyBill(String customerName);
+    Future<Void> payMyBill(String customerName);
 
     static BarmanService createProxy(Vertx vertx, String address) {
         return new BarmanServiceVertxEBProxy(vertx, address);
